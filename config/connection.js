f6d9453dfdc8ccfb4bb7608a7ac3f4ec79b7b652
Archer Whitesell Curry <awc1051@orad.northwestern.edu>
2023-11-15 09:18:34 -0600

    first commit

---
+++ config/connection.js
@@ -0,0 +1,21 @@
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);          //checks for the JAWSDB_URL and uses it if available; otherwise, it uses local host for database connection
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: '127.0.0.1',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
+}
+
+module.exports = sequelize;
---
+++ server.js
@@ -0,0 +1,22 @@
+// Dependencies
+const express = require("express");
+const exphbs = require("express-handlebars");
+const path = require("path");
+const hbs = exphbs.create({});
+
+// Sets up the Express App
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Set Handlebars as the default template engine.
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+app.use(express.static("images"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+// Starts the server to begin listening
+app.listen(PORT, () => {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
