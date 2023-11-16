9235e3dc88d4f65df4a5bb2d71c6fbb64cf972a6
Archer Whitesell Curry <awc1051@orad.northwestern.edu>
2023-11-16 12:11:45 -0600

    fixed seed data issue, still need to create table relationships

---
+++ models/index.js
@@ -0,0 +1,13 @@
+const Tank = require('./tank');
+
+const System = require('./system');
+const Column = require('./column');
+const Rack = require('./rack');
+
+const Strain = require('./strain');
+const Lab = require('./lab');
+const Fish = require('./fish');
+
+
+
+module.exports = { Tank, System, Column, Rack, Strain, Lab, Fish }
--- seeds/seed.js
+++ seeds/seed.js
@@ -1,21 +1,25 @@
 const sequelize = require("../config/connection");
-const { Lab, Strain, Tank, Column, Rack, System, Fish } = require("../models")
+const { Tank, System, Column, Rack, Strain, Lab, Fish } = require("../models/index.js")
 
-const systemData = require("./system")
-const columnData = require("./column")
-const rackData = require("./rack")
-const tankData = require("./tank")
-const strainData = require("./strain")
-const labData = require("./lab")
-const fishData = require("./fish")
+const tankData = require("./tank.json")
+
+const systemData = require("./system.json")
+const columnData = require("./column.json")
+const rackData = require("./rack.json")
+
+const strainData = require("./strain.json")
+const labData = require("./lab.json")
+const fishData = require("./fish.json")
 
 const seedDatabase = async () => {
     await sequelize.sync({ force: true });
 
+    const tank = await Tank.bulkCreate(tankData);
+
     const system = await System.bulkCreate(systemData);
     const column = await Column.bulkCreate(columnData);
     const rack = await Rack.bulkCreate(rackData);
-    const tank = await Tank.bulkCreate(tankData);
+
     const strain = await Strain.bulkCreate(strainData);
     const lab = await Lab.bulkCreate(labData);
     const fish = await Fish.bulkCreate(fishData);
